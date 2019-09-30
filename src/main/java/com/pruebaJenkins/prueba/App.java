84bd5e07014d8d6846c793851efa88e18f53ea4d
davidaguileraext <david.aguilera.ext@cetaqua.com>
2019-09-30 16:17:30 +0200

    Secon commit

--- src/main/java/com/pruebaJenkins/prueba/App.java
+++ src/main/java/com/pruebaJenkins/prueba/App.java
@@ -9,5 +9,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello crazy people!" );
+        System.out.println("How are locuelos?");
     }
 }
